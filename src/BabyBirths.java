e8394ec70d9ee1d26422d62821b5c79469e0ee22
Mike Brisson <brisson22@cogeco.ca>
2017-03-09 12:13:04 -0500

    first update

--- src/BabyBirths.java
+++ src/BabyBirths.java
@@ -6,6 +6,9 @@ public class BabyBirths {
 
     int totalBoys = 0;
     int totalGirls = 0;
+    int totalNames = 0;
+    int totalBoysNames = 0;
+    int totalGirlsNames = 0;
 
     public void printNames() {
         FileResource fr = new FileResource();
@@ -30,22 +33,28 @@ public class BabyBirths {
     public void totalBirths(FileResource fr) {
         int totalBirths = 0;
         for(CSVRecord record : fr.getCSVParser(false)) {
+            totalNames++;
             int numBorn = Integer.parseInt(record.get(2));
             totalBirths += numBorn;
             if (record.get(1).equalsIgnoreCase("M")) {
                 totalBoys += numBorn;
+                totalBoysNames++;
             } else {
                 totalGirls += numBorn;
+                totalGirlsNames++;
             }
         }
         System.out.println("Total Births = " + totalBirths);
         System.out.println("Total Boys = " + totalBoys);
         System.out.println("Total Girls = " + totalGirls);
+        System.out.println("The total number of names is: " + totalNames);
+        System.out.println("The total number of boys names is: " + totalBoysNames);
+        System.out.println("The total number of boys names is: " + totalGirlsNames);
     }
 
     public void testTotalBirths() {
 //        FileResource fr = new FileResource("data/example-small.csv");
-        FileResource fr = new FileResource("data/yob2014.csv");
+        FileResource fr = new FileResource("testing/yob2012short.csv");
         totalBirths(fr);
     }
 }
